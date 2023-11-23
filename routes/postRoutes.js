91c537ac9f600c7556ad3dcb3036f551cd0292ef
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-23 14:43:39 +0100

    FEAT add error catch

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,97 +1,125 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-const slugify = require('slugify'); // Correggi l'importazione della libreria
+const slugify = require('slugify');
 const router = express.Router();
 const prisma = new PrismaClient();
 
 // Endpoint per creare un nuovo post
 router.post('/posts', async (req, res) => {
-  const { title, content } = req.body;
+  try {
+    const { title, content } = req.body;
 
-  const slug = slugify(title, {
-    lower: true,
-    strict: true,
-  });
+    const slug = slugify(title, {
+      lower: true,
+      strict: true,
+    });
 
-  const newPost = await prisma.post.create({
-    data: {
-      title,
-      content,
-      slug,
-    },
-  });
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        content,
+        slug,
+      },
+    });
 
-  res.json(newPost);
+    res.json(newPost);
+  } catch (error) {
+    console.error("Errore durante la creazione del post:", error);
+    res.status(500).json({ error: "Errore durante la creazione del post" });
+  }
 });
-
 
 // Endpoint per recuperare un post utilizzando lo slug
 router.get('/posts/:slug', async (req, res) => {
-  const { slug } = req.params;
+  try {
+    const { slug } = req.params;
 
-  const post = await prisma.post.findUnique({
-    where: {
-      slug,
-    },
-  });
+    const post = await prisma.post.findUnique({
+      where: {
+        slug,
+      },
+    });
 
-  res.json(post);
+    if (!post) {
+      return res.status(404).json({ error: "Post non trovato" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error("Errore durante la ricerca del post:", error);
+    res.status(500).json({ error: "Errore durante la ricerca del post" });
+  }
 });
 
 // Endpoint per recuperare tutti i post con opzioni di filtro
 router.get('/posts', async (req, res) => {
-  const { published, keyword } = req.query;
+  try {
+    const { published, keyword } = req.query;
 
-  const posts = await prisma.post.findMany({
-    where: {
-      published: published === 'true',
-      OR: [
-        {
-          title: {
-            contains: keyword || '',
+    const posts = await prisma.post.findMany({
+      where: {
+        published: published === 'true',
+        OR: [
+          {
+            title: {
+              contains: keyword || '',
+            },
           },
-        },
-        {
-          content: {
-            contains: keyword || '',
+          {
+            content: {
+              contains: keyword || '',
+            },
           },
-        },
-      ],
-    },
-  });
+        ],
+      },
+    });
 
-  res.json(posts);
+    res.json(posts);
+  } catch (error) {
+    console.error("Errore durante il recupero dei post:", error);
+    res.status(500).json({ error: "Errore durante il recupero dei post" });
+  }
 });
 
 // Endpoint per aggiornare un post
 router.put('/posts/:slug', async (req, res) => {
-  const { slug } = req.params;
-  const { title, content } = req.body;
+  try {
+    const { slug } = req.params;
+    const { title, content } = req.body;
 
-  const updatedPost = await prisma.post.update({
-    where: {
-      slug,
-    },
-    data: {
-      title,
-      content,
-    },
-  });
+    const updatedPost = await prisma.post.update({
+      where: {
+        slug,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
 
-  res.json(updatedPost);
+    res.json(updatedPost);
+  } catch (error) {
+    console.error("Errore durante l'aggiornamento del post:", error);
+    res.status(500).json({ error: "Errore durante l'aggiornamento del post" });
+  }
 });
 
 // Endpoint per eliminare un post
 router.delete('/posts/:slug', async (req, res) => {
-  const { slug } = req.params;
+  try {
+    const { slug } = req.params;
 
-  const deletedPost = await prisma.post.delete({
-    where: {
-      slug,
-    },
-  });
+    const deletedPost = await prisma.post.delete({
+      where: {
+        slug,
+      },
+    });
 
-  res.json(deletedPost);
+    res.json(deletedPost);
+  } catch (error) {
+    console.error("Errore durante l'eliminazione del post:", error);
+    res.status(500).json({ error: "Errore durante l'eliminazione del post" });
+  }
 });
 
 module.exports = router;
